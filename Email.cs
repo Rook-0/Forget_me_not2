a1a780930aeeb276418e3b8d87e9ac821da7a105
Rouque <nospringer@gmail.com>
2016-12-07 15:21:43 -0800

    Simple Email Storage Object

---
+++ Email.cs
@@ -0,0 +1,13 @@
+namespace AbandonedCalls2
+{
+    public class Email
+    {
+        public string Body { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string CC { get; set; }
+        public string BCC { get; set; }
+        public string htmlTable { get; set; }
+
+    }
+}
